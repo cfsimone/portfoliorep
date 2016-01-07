d8a748ca9b96dbb910b9f67a87171dbad8e2dec9
simone <simone@headline-interactive.nl>
2016-01-07 16:50:06 +0100

    Files edited

--- html/wp-content/themes/simoneportfolio/functions.php
+++ html/wp-content/themes/simoneportfolio/functions.php
@@ -21,7 +21,7 @@ function wps_theme_styles() {
 
     wp_enqueue_style( 'muli', 'https://fonts.googleapis.com/css?family=Muli:400,700' );
 
-    wp_enqueue_style( 'muli', 'https://fonts.googleapis.com/css?family=Montserrat:400,700' );
+    wp_enqueue_style( 'montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:400,700' );
 
     wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 }
