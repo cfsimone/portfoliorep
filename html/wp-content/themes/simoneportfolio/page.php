38426b5f3d1960f8076f134e3a80b382035c0b78
simone <simone@headline-interactive.nl>
2016-01-14 16:29:19 +0100

    Files edited

--- html/wp-content/themes/simoneportfolio/page.php
+++ html/wp-content/themes/simoneportfolio/page.php
@@ -1,33 +1,19 @@
-<?php
-///*
-//    Template name: Portfolio
-//*/
-//?>
-<!---->
-<?php //get_header(); ?>
-<!---->
-<!---->
-<!--<div class="main__content">-->
-<!---->
-<!--    --><?php //if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<!---->
-<!--        <h1 class="main__header">--><?php //the_title(); ?><!--</h1>-->
-<!--        --><?php //the_content(); ?>
-<!---->
-<!--            --><?php
-//            args = array(
-//                'post-type' => 'portfolio'
-//                );
-//            $query = new WP_Query( $args);
-//            ?>
-<!---->
-<!---->
-<!---->
-<!---->
-<!--    --><?php //endwhile; endif; ?>
-<!---->
-<!--</div>-->
-<!---->
-<?php //get_footer(); ?>
-<!---->
-<!---->
+<?php get_header(); ?>
+
+
+<div class="main__content">
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h1 class="main__header"><?php the_title(); ?></h1>
+        <p><?php the_content(); ?></p>
+
+    <?php endwhile; else : ?>
+
+        <p><?php _e( 'Sorry, no pages found' ); ?></p>
+
+    <?php endif; ?>
+
+</div>
+<hr>
+<?php get_footer(); ?>
